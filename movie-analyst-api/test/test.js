dba3ce67b2a9d3ea66256b385b978592af677f32
jaiber diaz <djaiver9@gmail.com>
2025-10-21 22:55:00 -0500

    update import modules and add endpoint tests

--- movie-analyst-api/test/test.js
+++ movie-analyst-api/test/test.js
@@ -1,12 +1,19 @@
 process.env.NODE_ENV = 'test';
 
-//Require the dev-dependencies
+// Import dev-dependencies using ES6 syntax
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import server from '../server.js';  // ← Add .js extension for ES modules
+
+/*legacy code for requiring the dev-dependencies
 let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../server');
-let should = chai.should();
+let should = chai.should();*/
 
+const should = chai.should();
 chai.use(chaiHttp);
+
 //Our parent block
 describe('API', () => {
 
@@ -18,10 +25,71 @@ describe('API', () => {
         chai.request(server)
             .get('/')
             .end((err, res) => {
-                res.should.have.status(200);
+              if(err){
+                done(err);
+              }
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+              res.body.should.have.property('service_status').eql('Up');
               done();
             });
       });
   });
 
 });
+
+  /*
+    * Test the /GET movies route
+    */
+  describe('/GET movies', () => {
+      it('it should GET all movies', (done) => {
+        chai.request(server)
+          .get('/movies')
+          .end((err, res) => {
+            if (err) {
+              done(err);
+              return;
+            }
+            res.should.have.status(200);
+            res.body.should.be.a('array');
+            done();
+          });
+      });
+    });
+
+  /*
+  * Test the /GET reviewers route
+  */
+   describe('/GET reviewers', () => {
+    it('it should GET all reviewers', (done) => {
+      chai.request(server)
+        .get('/reviewers')
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          done();
+        });
+    });
+  });
+  /*
+  * Test the /GET publications route
+  */
+   describe('/GET publications', () => {
+    it('it should GET all publications', (done) => {
+      chai.request(server)
+        .get('/publications')
+        .end((err, res) => {
+          if (err) {
+            done(err);
+            return;
+          }
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          done();
+        });
+    });
+  });
